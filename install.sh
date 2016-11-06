79d24705b8a81a9a06be456354ded0ea88955b56
Ocraz <registered@quicknet.nl>
2016-11-06 13:29:41 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
-Folder="/opt/gits"
+add_git_folder="/opt/gits"
+version_notepadqq="0.53.0-0~yakkety1"
+echo ===============================================
+echo === Installing NotepadQQ $version_notepadqq ====
+echo ===============================================
+cd /tmp
+wget "https://launchpad.net/~notepadqq-team/+archive/ubuntu/notepadqq/+files/notepadqq-common_"$version_notepadqq"_all.deb"
+wget "https://launchpad.net/~notepadqq-team/+archive/ubuntu/notepadqq/+files/notepadqq-gtk_"$version_notepadqq"_all.deb"
+sudo dpkg -i "notepadqq-common_"$version_notepadqq"_all.deb"; sudo apt-get install -f
+sudo dpkg -i "notepadqq-gtk_"$version_notepadqq"_all.deb"; sudo apt-get install -f
+rm "/tmp/notepadqq-common_"$version_notepadqq"_all.deb"
+rm "/tmp/notepadqq-gtk_"$version_notepadqq"_all.deb"
+echo ====================================================
+echo === Done installing NotepadQQ $version_notepadqq ====
+echo ====================================================
 
-cd $Folder
+
+cd $add_git_folder
 
 #git pull
